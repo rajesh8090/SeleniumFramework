1b576f75612826a1a262c8d816614f9815ea44af
hp <hp@hp-pc>
2020-01-30 10:23:26 +0530

    raj first commit

---
+++ testGit/src/assrt/DemoSoftassert.java
@@ -0,0 +1,23 @@
+package assrt;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class DemoSoftassert
+{
+@Test
+public void m1()
+{
+	String s1="araj";
+	String s2="raj";
+	
+	System.out.println("m1 start");
+	SoftAssert sa=new SoftAssert();
+	sa.assertEquals(s1, s2);
+	
+	System.out.println("end");
+	sa.assertAll();
+	
+}
+}
